6b8f4a99107908ca435cca030e34564ddf4c6b2c
simona.loperfido <sloperfido96@gmail.com>
2021-09-22 14:44:32 +0200

    tutorial completato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AdminDashboard';
+  title = 'admin-dashboard';
+  sideBarOpen = true;
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
